efcf8b19ea2d70d2fc451c058556d954bf5a12be
chet <chet>
2004-08-05 00:43:54 +0000

    New update, 2004/08/05

--- migueloo/var/themes/Miguel/js/menu.js
+++ migueloo/var/themes/Miguel/js/menu.js
@@ -1,182 +1,322 @@
+// Title: tigra menu
+// Description: See the demo at url
+// URL: http://www.softcomplex.com/products/tigra_menu/
+// Version: 2.0 (commented source)
+// Date: 04-05-2003 (mm-dd-yyyy)
+// Tech. Support: http://www.softcomplex.com/forum/forumdisplay.php?fid=40
+// Notes: This script is free. Visit official site for further details.
 
-//Pop-it menu- By Dynamic Drive
-//For full source code and more DHTML scripts, visit http://www.dynamicdrive.com
-//This credit MUST stay intact for use
+// --------------------------------------------------------------------------------
+// global collection containing all menus on current page
+var A_MENUS = [];
 
-var linkset=new Array()
-//SPECIFY MENU SETS AND THEIR LINKS. FOLLOW SYNTAX LAID OUT
+// --------------------------------------------------------------------------------
+// menu class
+function menu (a_items, a_tpl) {
 
-//linkset[0]='<div class="menuitems"><a href="http://www.precios10.com">Ordenadores</a>'
-//linkset[0]+='<a href="http://www.cambiabanners.com">Intercambio de banners</a>'
-//linkset[0]+='<a href="http://www.iaupa.com">Recursos web</a></div>'
+	// browser check
+	if (!document.body || !document.body.style)
+		return;
 
-linkset='';
+	// store items structure
+	this.a_config = a_items;
 
-//linkset[1]='<div class="menuitems"><a href="http://msnbc.com">MSNBC</a></div>'
-//linkset[1]+='<div class="menuitems"><a href="http://cnn.com">CNN</a></div>'
-//linkset[1]+='<div class="menuitems"><a href="http://abcnews.com">ABC News</a></div>'
-//linkset[1]+='<div class="menuitems"><a href="http://www.washingtonpost.com">Washington Post</a></div>'
+	// store template structure
+	this.a_tpl = a_tpl;
 
-////No need to edit beyond here
+	// get menu id
+	this.n_id = A_MENUS.length;
 
-var ie4=document.all&&navigator.userAgent.indexOf("Opera")==-1
-var ns6=document.getElementById&&!document.all
-var ns4=document.layers
+	// declare collections
+	this.a_index = [];
+	this.a_children = [];
 
-function cleanref()
-{
-	//Como tabla
-	//linkset = '<div class="menuitems" style="overflow:visible"><table><tr>'
-	//Por columnas
-	//linkset = '<div class="menuitems" style="overflow:visible">'
-	//linkset = '<div class="menuitems">'
-	//Por filas
-	//linkset = ''
+	// assigh methods and event handlers
+	this.expand      = menu_expand;
+	this.collapse    = menu_collapse;
+
+	this.onclick     = menu_onclick;
+	this.onmouseout  = menu_onmouseout;
+	this.onmouseover = menu_onmouseover;
+	this.onmousedown = menu_onmousedown;
+
+	// default level scope description structure 
+	this.a_tpl_def = {
+		'block_top'  : 16,
+		'block_left' : 16,
+		'top'        : 20,
+		'left'       : 4,
+		'width'      : 120,
+		'height'     : 22,
+		'hide_delay' : 0,
+		'expd_delay' : 0,
+		'css'        : {
+			'inner' : '',
+			'outer' : ''
+		}
+	};
 	
-	linkset='<table width="100%" border="0" cellspacing="1" cellpadding="0" class="mainInterfaceWidth"><tr>'
+	// assign methods and properties required to imulate parent item
+	this.getprop = function (s_key) {
+		return this.a_tpl_def[s_key];
+	};
+
+	this.o_root = this;
+	this.n_depth = -1;
+	this.n_x = 0;
+	this.n_y = 0;
+
+	// 	init items recursively
+	for (n_order = 0; n_order < a_items.length; n_order++)
+		new menu_item(this, n_order);
+
+	// register self in global collection
+	A_MENUS[this.n_id] = this;
+
+	// make root level visible
+	for (var n_order = 0; n_order < this.a_children.length; n_order++)
+		this.a_children[n_order].e_oelement.style.visibility = 'visible';
 }
 
-function addref(href, name, superior)
-{
-	//Por columnas	
-	//linkset += '<a href="' + href + '" onmouseover="highlightmenu(event,' + "'on'," + superior + ')"' + 'onmouseout="highlightmenu(event,' + "'off'," + superior + ')">' + name + '</a>'
-	linkset += '<td class="menuitems" align="center" onmouseover="highlightmenu(event,' + "'on'," + superior + ')"' + 'onmouseout="highlightmenu(event,' + "'off'," + superior + ')"><a href="' + href + '">' + name + '</a></td>'
+// --------------------------------------------------------------------------------
+function menu_collapse (n_id) {
+	// cancel item open delay
+	clearTimeout(this.o_showtimer);
 
-	//Por filas
-	//linkset += '<div class="menuitems" ><a href="' + href + '" onmouseover="highlightmenu(event,' + "'on'," + superior + ')"' + 'onmouseout="highlightmenu(event,' + "'off'," + superior + ')">' + name + '</a></div>'
-	//Como tabla
-	//linkset += '<td><a href="' + href + '" onmouseover="highlightmenu(event,' + "'on'," + superior + ')"' + 'onmouseout="highlightmenu(event,' + "'off'," + superior + ')">' + name + '</a></td>'
-	// linkset += '<td><a href="' + href + '">' + name + '</a></td>'
-}
+	// by default collapse to root level
+	var n_tolevel = (n_id ? this.a_index[n_id].n_depth : 0);
 	
+	// hide all items over the level specified
+	for (n_id = 0; n_id < this.a_index.length; n_id++) {
+		var o_curritem = this.a_index[n_id];
+		if (o_curritem.n_depth > n_tolevel && o_curritem.b_visible) {
+			o_curritem.e_oelement.style.visibility = 'hidden';
+			o_curritem.b_visible = false;
+		}
+	}
 
-function closeref()
-{
-	//Por filas
-	//linkset += '</div>'
-	//Como tabla
-	//linkset += '</tr></table></div>'
-	linkset += '</tr></table>'
+	// reset current item if mouse has gone out of items
+	if (!n_id)
+		this.o_current = null;
 }
 
-function showmenu(e,which,superior){
+// --------------------------------------------------------------------------------
+function menu_expand (n_id) {
 
-if (!document.all&&!document.getElementById&&!document.layers)
-return
+	// expand only when mouse is over some menu item
+	if (this.o_hidetimer)
+		return;
 
-clearhidemenu()
+	// lookup current item
+	var o_item = this.a_index[n_id];
 
-if (superior)
-{
-	menuobj=ie4? document.all.superior : ns6? document.getElementById("superior") : ns4? document.popmenu : ""
-}
-else
-{
-	menuobj=ie4? document.all.inferior : ns6? document.getElementById("inferior") : ns4? document.popmenu : ""
-}
-menuobj.thestyle=(ie4||ns6)? menuobj.style : menuobj
+	// close previously opened items
+	if (this.o_current && this.o_current.n_depth >= o_item.n_depth)
+		this.collapse(o_item.n_id);
+	this.o_current = o_item;
 
-if (ie4||ns6)
-{
-//	for (i=0; i<texts.length;i++)
-//	{
-		//innerH +='<div class="menuitems"><a href="www.polar.es">' + texts[i] + '</a></div>'
-		menuobj.innerHTML=which
-//	}
-//menuobj.innerHTML=which
-}
-else{
-menuobj.document.write('<layer name=gui bgColor=#E6E6E6 width=165 onmouseover="clearhidemenu()" onmouseout="hidemenu()">'+which+'</layer>')
-menuobj.document.close()
+	// exit if there are no children to open
+	if (!o_item.a_children)
+		return;
+
+	// show direct child items
+	for (var n_order = 0; n_order < o_item.a_children.length; n_order++) {
+		var o_curritem = o_item.a_children[n_order];
+		o_curritem.e_oelement.style.visibility = 'visible';
+		o_curritem.b_visible = true;
+	}
 }
 
-/*
-menuobj.contentwidth=(ie4||ns6)? menuobj.offsetWidth : menuobj.document.gui.document.width
-menuobj.contentheight=(ie4||ns6)? menuobj.offsetHeight : menuobj.document.gui.document.height
-eventX=ie4? event.clientX : ns6? e.clientX : e.x
-eventY=ie4? event.clientY : ns6? e.clientY : e.y
-
-
-//Find out how close the mouse is to the corner of the window
-
-var rightedge=ie4? document.body.clientWidth-eventX : window.innerWidth-eventX
-var bottomedge=ie4? document.body.clientHeight-eventY : window.innerHeight-eventY
-
-//if the horizontal distance isn't enough to accomodate the width of the context menu
-if (rightedge<menuobj.contentwidth)
-//move the horizontal position of the menu to the left by it's width
-menuobj.thestyle.left=ie4? document.body.scrollLeft+eventX-menuobj.contentwidth : ns6? window.pageXOffset+eventX-menuobj.contentwidth : eventX-menuobj.contentwidth
-else
-//position the horizontal position of the menu where the mouse was clicked
-menuobj.thestyle.left=ie4? document.body.scrollLeft+eventX : ns6? window.pageXOffset+eventX : eventX
-
-//same concept with the vertical position
-if (bottomedge<menuobj.contentheight)
-menuobj.thestyle.top=ie4? document.body.scrollTop+eventY-menuobj.contentheight : ns6? window.pageYOffset+eventY-menuobj.contentheight : eventY-menuobj.contentheight
-else
-menuobj.thestyle.top=ie4? document.body.scrollTop+event.clientY : ns6? window.pageYOffset+eventY : eventY
-*/
-menuobj.thestyle.visibility="visible"
-return false
+// --------------------------------------------------------------------------------
+//
+// --------------------------------------------------------------------------------
+function menu_onclick (n_id) {
+	// don't go anywhere if item has no link defined
+	return Boolean(this.a_index[n_id].a_config[1]);
 }
 
-function contains_ns6(a, b) {
-//Determines if 1 element in contained in another- by Brainjar.com
-while (b.parentNode)
-if ((b = b.parentNode) == a)
-return true;
-return false;
+// --------------------------------------------------------------------------------
+function menu_onmouseout (n_id) {
+
+	// lookup new item's object	
+	var o_item = this.a_index[n_id];
+
+	// apply rollout
+	o_item.e_oelement.className = o_item.getstyle(0, 0);
+	o_item.e_ielement.className = o_item.getstyle(1, 0);
+	
+	// update status line	
+	o_item.upstatus(7);
+
+	// run mouseover timer
+	this.o_hidetimer = setTimeout('A_MENUS['+ this.n_id +'].collapse();',
+		o_item.getprop('hide_delay'));
 }
 
-function hidemenu(){
-	if (window.menuobj)
-		menuobj.thestyle.visibility=(ie4||ns6)? "hidden" : "hide"
+// --------------------------------------------------------------------------------
+function menu_onmouseover (n_id) {
+
+	// cancel mouseoute menu close and item open delay
+	clearTimeout(this.o_hidetimer);
+	this.o_hidetimer = null;
+	clearTimeout(this.o_showtimer);
+
+	// lookup new item's object	
+	var o_item = this.a_index[n_id];
+
+	// update status line	
+	o_item.upstatus();
+
+	// apply rollover
+	o_item.e_oelement.className = o_item.getstyle(0, 1);
+	o_item.e_ielement.className = o_item.getstyle(1, 1);
+	
+	// if onclick open is set then no more actions required
+	if (o_item.getprop('expd_delay') < 0)
+		return;
+
+	// run expand timer
+	this.o_showtimer = setTimeout('A_MENUS['+ this.n_id +'].expand(' + n_id + ');',
+		o_item.getprop('expd_delay'));
+
 }
 
-function dynamichide(e){
-if (ie4&&!menuobj.contains(e.toElement))
-hidemenu()
-else if (ns6&&e.currentTarget!= e.relatedTarget&& !contains_ns6(e.currentTarget, e.relatedTarget))
-hidemenu()
+// --------------------------------------------------------------------------------
+// called when mouse button is pressed on menu item
+// --------------------------------------------------------------------------------
+function menu_onmousedown (n_id) {
+	
+	// lookup new item's object	
+	var o_item = this.a_index[n_id];
+
+	// apply mouse down style
+	o_item.e_oelement.className = o_item.getstyle(0, 2);
+	o_item.e_ielement.className = o_item.getstyle(1, 2);
+
+	this.expand(n_id);
+//	this.items[id].switch_style('onmousedown');
 }
 
-function delayhidemenu(){
-if (ie4||ns6||ns4)
-delayhide=setTimeout("hidemenu()",200)
+
+// --------------------------------------------------------------------------------
+// menu item Class
+function menu_item (o_parent, n_order) {
+
+	// store parameters passed to the constructor
+	this.n_depth  = o_parent.n_depth + 1;
+	this.a_config = o_parent.a_config[n_order + (this.n_depth ? 3 : 0)];
+
+	// return if required parameters are missing
+	if (!this.a_config) return;
+
+	// store info from parent item
+	this.o_root    = o_parent.o_root;
+	this.o_parent  = o_parent;
+	this.n_order   = n_order;
+
+	// register in global and parent's collections
+	this.n_id = this.o_root.a_index.length;
+	this.o_root.a_index[this.n_id] = this;
+	o_parent.a_children[n_order] = this;
+
+	// calculate item's coordinates
+	var o_root = this.o_root,
+		a_tpl  = this.o_root.a_tpl;
+
+	// assign methods
+	this.getprop  = mitem_getprop;
+	this.getstyle = mitem_getstyle;
+	this.upstatus = mitem_upstatus;
+
+	this.n_x = n_order
+		? o_parent.a_children[n_order - 1].n_x + this.getprop('left')
+		: o_parent.n_x + this.getprop('block_left');
+
+	this.n_y = n_order
+		? o_parent.a_children[n_order - 1].n_y + this.getprop('top')
+		: o_parent.n_y + this.getprop('block_top');
+
+	// generate item's HMTL
+	document.write (
+		'<a id="e' + o_root.n_id + '_'
+			+ this.n_id +'o" class="' + this.getstyle(0, 0) + '" href="' + this.a_config[1] + '"'
+			+ (this.a_config[2] && this.a_config[2]['tw'] ? ' target="'
+			+ this.a_config[2]['tw'] + '"' : '') + ' style="position: absolute; top: '
+			+ this.n_y + 'px; left: ' + this.n_x + 'px; width: '
+			+ this.getprop('width') + 'px; height: '
+			+ this.getprop('height') + 'px; visibility: hidden;'
+			+' z-index: ' + this.n_depth + ';" '
+			+ 'onclick="return A_MENUS[' + o_root.n_id + '].onclick('
+			+ this.n_id + ');" onmouseout="A_MENUS[' + o_root.n_id + '].onmouseout('
+			+ this.n_id + ');" onmouseover="A_MENUS[' + o_root.n_id + '].onmouseover('
+			+ this.n_id + ');" onmousedown="A_MENUS[' + o_root.n_id + '].onmousedown('
+			+ this.n_id + ');"><div  id="e' + o_root.n_id + '_'
+			+ this.n_id +'i" class="' + this.getstyle(1, 0) + '">'
+			+ this.a_config[0] + "</div></a>\n"
+		);
+	this.e_ielement = document.getElementById('e' + o_root.n_id + '_' + this.n_id + 'i');
+	this.e_oelement = document.getElementById('e' + o_root.n_id + '_' + this.n_id + 'o');
+
+	this.b_visible = !this.n_depth;
+
+	// no more initialization if leaf
+	if (this.a_config.length < 4)
+		return;
+
+	// node specific methods and properties
+	this.a_children = [];
+
+	// init downline recursively
+	for (var n_order = 0; n_order < this.a_config.length - 3; n_order++)
+		new menu_item(this, n_order);
 }
 
-function clearhidemenu(){
-if (window.delayhide)
-clearTimeout(delayhide)
+// --------------------------------------------------------------------------------
+// reads property from template file, inherits from parent level if not found
+// ------------------------------------------------------------------------------------------
+function mitem_getprop (s_key) {
+
+	// check if value is defined for current level
+	var s_value = null,
+		a_level = this.o_root.a_tpl[this.n_depth];
+
+	// return value if explicitly defined
+	if (a_level)
+		s_value = a_level[s_key];
+
+	// request recursively from parent levels if not defined
+	return (s_value == null ? this.o_parent.getprop(s_key) : s_value);
+}
+// --------------------------------------------------------------------------------
+// reads property from template file, inherits from parent level if not found
+// ------------------------------------------------------------------------------------------
+function mitem_getstyle (n_pos, n_state) {
+
+	var a_css = this.getprop('css');
+	var a_oclass = a_css[n_pos ? 'inner' : 'outer'];
+
+	// same class for all states	
+	if (typeof(a_oclass) == 'string')
+		return a_oclass;
+
+	// inherit class from previous state if not explicitly defined
+	for (var n_currst = n_state; n_currst >= 0; n_currst--)
+		if (a_oclass[n_currst])
+			return a_oclass[n_currst];
 }
 
-function highlightmenu(e,state,superior){
-if (document.all)
-source_el=event.srcElement
-else if (document.getElementById)
-source_el=e.target
-if (source_el.className=="menuitems"){
-source_el.id=(state=="on")? "mouseoverstyle" : ""
-}
-else{
-if (superior==1)
-{
-	popupname="superior"
-}
-else
-{
-	popupname="inferior"
-}
-while(source_el.id!=popupname){
-source_el=document.getElementById? source_el.parentNode : source_el.parentElement
-if (source_el.className=="menuitems"){
-source_el.id=(state=="on")? "mouseoverstyle" : ""
-}
-}
-}
+// ------------------------------------------------------------------------------------------
+// updates status bar message of the browser
+// ------------------------------------------------------------------------------------------
+function mitem_upstatus (b_clear) {
+	window.setTimeout("window.status=unescape('" + (b_clear
+		? ''
+		: (this.a_config[2] && this.a_config[2]['sb']
+			? escape(this.a_config[2]['sb'])
+			: escape(this.a_config[0]) + (this.a_config[1]
+				? ' ('+ escape(this.a_config[1]) + ')'
+				: ''))) + "')", 10);
 }
 
-if (ie4||ns6)
-document.onclick=hidemenu
-
-
+// --------------------------------------------------------------------------------
+// that's all folks
